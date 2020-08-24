9181732fa1c3e11001dd3b7a9b1c6bad4be60701
Luke Tchang <ltchang@stanford.edu>
2020-08-23 20:43:33 -0700

    initial commit

---
+++ event-bus/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const events = [];
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    events.push(event);
+    
+    axios.post('http://localhost:4000/events', event); //posts
+    axios.post('http://localhost:4001/events', event); //comments
+    axios.post('http://localhost:4002/events', event); //query
+    axios.post('http://localhost:4003/events', event); //moderation
+
+    res.send({ status: "OK" });
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
+})
+
+app.listen(4005, () => {
+    console.log("Listening on port 4005 (event bus)...");
+});
